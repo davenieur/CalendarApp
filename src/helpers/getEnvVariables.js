34d4832b533c7184153172de0094d81ad8ff422f
L00vely <peralesrodriguezdavid@gmail.com>
2023-07-05 15:50:48 -0600

    Corrgiendo para el deploy

--- src/helpers/getEnvVariables.js
+++ src/helpers/getEnvVariables.js
@@ -1,7 +1,7 @@
-export const getEnvVariables = () => {
-    import.meta.env;
+// export const getEnvVariables = () => {
+//     import.meta.env;
 
-    return {
-        ...import.meta.env
-    }
-}
+//     return {
+//         ...import.meta.env
+//     }
+// }
